3061686b882a3743f77d7535315015e2389faed3
2048-cupcake <93824425+2048-cupcake@users.noreply.github.com>
2022-10-04 02:48:37 +0530

    Update specialgame.js

--- json/specialgame.js
+++ json/specialgame.js
@@ -1,13 +1,15 @@
-	 var xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function() {
-		if(this.readyState == 4 && this.status == 200) {
-	    var response = JSON.parse(xhttp.responseText);
-		var user = response.user;
-		var showdata = "";
-for (var i = user.length - 1; i >= user.length-10; i--){
-showdata += '<li><a href="' + user[i].websiteurl + '"   target="_blank" data-overlay-text="' + user[i].gamename + '"  class="overlay-container" >	<img src="' + user[i].image + '"  class="overlay-img"   alt="2048 Classic"> </a>';    //<h2 class="gamename">'+user[i].gamename+ '</h2>';  
-showdata += ' </li>';
-}
-document.write(showdata);
-}};
-var _0x3d51=["\x47\x45\x54","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x32\x30\x34\x38\x2D\x63\x75\x70\x63\x61\x6B\x65\x2E\x67\x69\x74\x68\x75\x62\x2E\x69\x6F\x2F\x32\x30\x34\x38\x67\x61\x6D\x65\x2F\x6A\x73\x6F\x6E\x2F\x73\x70\x65\x63\x69\x61\x6C\x67\x61\x6D\x65\x2E\x6A\x73\x6F\x6E","\x6F\x70\x65\x6E","\x73\x65\x6E\x64"];xhttp[_0x3d51[2]](_0x3d51[0],_0x3d51[1],false);xhttp[_0x3d51[3]]()
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+if(this.readyState == 4 && this.status == 200) {
+var response = JSON.parse(xhttp.responseText);
+var user = response.user;
+var showdata = "";
+for (var i = user.length - 1; i >1; i--){
+showdata += ' <a href=' + user[i].websiteurl + ' target=\"_blank\">';  
+showdata += '<div class="item">';
+showdata += '<div><img src="' + user[i].image  + '"    alt="2048 Classic" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" width="400" height="400" loading="lazy"> </div>';
+showdata += '<div>' + user[i].gamename  + '</div>';
+showdata += '</div>';
+showdata += '</a>';}
+document.write(showdata);}}; 
+var _0x7e15=["\x47\x45\x54","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x32\x30\x34\x38\x2D\x63\x75\x70\x63\x61\x6B\x65\x2E\x67\x69\x74\x68\x75\x62\x2E\x69\x6F\x2F\x32\x30\x34\x38\x67\x61\x6D\x65\x2F\x6A\x73\x6F\x6E\x2F\x73\x70\x65\x63\x69\x61\x6C\x67\x61\x6D\x65\x2E\x6A\x73\x6F\x6E","\x6F\x70\x65\x6E","\x73\x65\x6E\x64"];xhttp[_0x7e15[2]](_0x7e15[0],_0x7e15[1],false);xhttp[_0x7e15[3]]()
