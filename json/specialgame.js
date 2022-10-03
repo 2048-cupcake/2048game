147cd227a82d45663a20a5b7ad1c2c66243fbd06
2048-cupcake <93824425+2048-cupcake@users.noreply.github.com>
2022-10-04 02:29:37 +0530

    Update specialgame.js

--- json/specialgame.js
+++ json/specialgame.js
@@ -1,56 +1,13 @@
-		var xhttp = new XMLHttpRequest();
+	 var xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function() {
 		if(this.readyState == 4 && this.status == 200) {
 	    var response = JSON.parse(xhttp.responseText);
 		var user = response.user;
 		var showdata = "";
-			//for(var i = 0; i < user.length; i++) {
-                 for(var i = 0; i <=2; i++) { 
-                  //for (var i = user.length - 1; i >= 0; i--){
-					//showdata += ' <div id=' + user[i].url + ' class="userpost">';
-		           // showdata += '<div>';
-
-//showdata += '<div class="main-container">';
-//showdata += '<ul class="grid-wrapper">';
-//showdata += '<li><a href="' + user[i].websiteurl + '"   target="_blank">	<img src="' + user[i].image + '"    alt="2048 Classic"> </a><h2 class="gamename">'+user[i].gamename+ '</h2>';
-  
-  
- //showdata += ' </li>';
- 
- 
- 
- 
- showdata += ' <a href=' + user[i].websiteurl + ' target=\"_blank\">';  
- 
- 
- //' + user[i].FileName+ ' <br> <i class="fa fa-download" style="font-size:20px;color:#fff;"></i>&nbsp;&nbsp;<strong>Download</strong>';
-	
-	showdata += '<div class="item">';
-	
-	showdata += '<div><img src="' + user[i].image  + '"    alt="2048 Classic" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" width="400" height="400" loading="lazy"> </div>';
-				
-				
-				
-				showdata += '<div>' + user[i].gamename  + '</div>';
-	
-	showdata += '</div>';
-	showdata += '</a>';
- 
- 
- 
-//showdata += '</ul>';
-//showdata += '</div>';
-					//showdata += '</div>';
-					//showdata += '</div>';
-								
-									
-							}
-							document.write(showdata);
-						}
-					}; 
-  xhttp.open("GET", "https://2048-cupcake.github.io/2048game/json/specialgame.json", false);	
- //xhttp.open("GET", "https://2048-cupcake.github.io/2048game/js/allgames.json", false);		   
-         xhttp.send(); 
-//xhttp.open("GET", "https://script.google.com/macros/s/AKfycbyjDXNgxpwwff3qdHSpa4Q5kwgBn6SugZV9NVPH1iBjUx-gI822oOMwz-Xis-B2O6M6wQ/exec", false);					
-					//xhttp.send();  
-/* var _0x6a76=["\x47\x45\x54","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x73\x63\x72\x69\x70\x74\x2E\x67\x6F\x6F\x67\x6C\x65\x2E\x63\x6F\x6D\x2F\x6D\x61\x63\x72\x6F\x73\x2F\x73\x2F\x41\x4B\x66\x79\x63\x62\x78\x51\x6C\x65\x46\x59\x75\x63\x72\x44\x69\x72\x62\x4E\x37\x74\x68\x65\x73\x6E\x57\x49\x70\x31\x48\x5A\x51\x6F\x5F\x75\x6E\x41\x2D\x5A\x39\x31\x52\x4D\x78\x6F\x6A\x51\x4D\x64\x68\x76\x76\x67\x43\x5A\x4A\x51\x31\x6F\x46\x4C\x38\x70\x75\x50\x71\x4E\x4B\x43\x61\x42\x39\x41\x2F\x65\x78\x65\x63","\x6F\x70\x65\x6E","\x73\x65\x6E\x64"];xhttp[_0x6a76[2]](_0x6a76[0],_0x6a76[1],false);xhttp[_0x6a76[3]]() */
+for (var i = user.length - 1; i >= user.length-10; i--){
+showdata += '<li><a href="' + user[i].websiteurl + '"   target="_blank" data-overlay-text="' + user[i].gamename + '"  class="overlay-container" >	<img src="' + user[i].image + '"  class="overlay-img"   alt="2048 Classic"> </a>';    //<h2 class="gamename">'+user[i].gamename+ '</h2>';  
+showdata += ' </li>';
+}
+document.write(showdata);
+}};
+var _0x3d51=["\x47\x45\x54","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x32\x30\x34\x38\x2D\x63\x75\x70\x63\x61\x6B\x65\x2E\x67\x69\x74\x68\x75\x62\x2E\x69\x6F\x2F\x32\x30\x34\x38\x67\x61\x6D\x65\x2F\x6A\x73\x6F\x6E\x2F\x73\x70\x65\x63\x69\x61\x6C\x67\x61\x6D\x65\x2E\x6A\x73\x6F\x6E","\x6F\x70\x65\x6E","\x73\x65\x6E\x64"];xhttp[_0x3d51[2]](_0x3d51[0],_0x3d51[1],false);xhttp[_0x3d51[3]]()
